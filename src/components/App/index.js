2bb22d6dc284e6c1b182ac0faa83ff31d738eb94
Artur <grandfaraon93@gmail.com>
2019-11-21 15:05:52 +0200

    Router update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/ReactStockReports/">
           <Switch>
             <Route exact path='/' component={HomePage} />
           </Switch>

0836ab130b2dcc45c3deb8dcd4c58867834562a0
Artur <grandfaraon93@gmail.com>
2019-11-25 16:47:29 +0200

    Name update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter basename="/ReactStockReports/">
+        <BrowserRouter basename="/IexcloudReactApi/">
           <Switch>
             <Route exact path='/' component={HomePage} />
           </Switch>

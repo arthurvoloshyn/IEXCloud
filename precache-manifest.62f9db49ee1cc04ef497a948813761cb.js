aec63ed58ea21ee84bde0a596762c64b37c58d6e
Artur <grandfaraon93@gmail.com>
2020-01-27 18:36:29 +0200

    Updates

---
+++ precache-manifest.62f9db49ee1cc04ef497a948813761cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4a4590d15757853e12a03bd075e8f9",
+    "url": "/IEXCloud/index.html"
+  },
+  {
+    "revision": "6aa62726df2fa50d7cf4",
+    "url": "/IEXCloud/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "502ad5b6e4fc95a753d1",
+    "url": "/IEXCloud/static/css/main.02c1077c.chunk.css"
+  },
+  {
+    "revision": "6aa62726df2fa50d7cf4",
+    "url": "/IEXCloud/static/js/2.6cae4030.chunk.js"
+  },
+  {
+    "revision": "502ad5b6e4fc95a753d1",
+    "url": "/IEXCloud/static/js/main.ab437d3c.chunk.js"
+  },
+  {
+    "revision": "8fc51fe5572b9f8fba7a",
+    "url": "/IEXCloud/static/js/runtime~main.2e0908c3.js"
+  }
+]);
